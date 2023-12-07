166d6cf5cf714610a490d5b21cbacf508c01ec35
heitorleonny <heitorleonnyyt1234@gmail.com>
2023-12-06 23:03:06 -0300

    add initial page

--- type_event/urls.py
+++ type_event/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuarios/', include('usuarios.urls')),
+    path('', include('usuarios.urls')),
     path('eventos/', include('eventos.urls')),
     path('cliente/', include('cliente.urls')),
     
